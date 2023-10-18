e906e182e4cec875ece4fe5f89e9aeff6f01d47f
Milan Sobota <milanh.b.sobota@gmail.com>
2023-10-18 11:52:36 +0100

    Moved basic model to model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -8,24 +8,26 @@ class Model:
     Parameters
     ----------
 
-    value: numeric, optional
+    Q_p1: numeric, optional
+        the transition rate between central compartment and peripheral compartment 1
+    V_c: numeric, optional
+        [mL], the volume of the central compartment
+    V_p1: numeric, optional
+        [mL], the volume of the first peripheral compartment
+    CL: numeric, optional
+        [mL/h], the clearance/elimination rate from the central compartment
+    X: numeric, optional
         an example paramter
+    
 
     """
 
-    model1_args = {
-        'name': 'model1',
-        'Q_p1': 1.0,
-        'V_c': 1.0,
-        'V_p1': 1.0,
-        'CL': 1.0,
-        'X': 1.0,
-    }
-
-    def __init__(self, Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, X = 1.0):
-        self.value = value
-        model1_args = {
-            'name': 'model1',
+    def __init__(self, Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, X = 1.0, delivery = "intravenous"):
+        delivery_types = ['intravenous', 'subcutaneous']
+        if delivery not in delivery_types:
+            raise ValueError("Invalid delivery type. Expected one of: %s" % delivery_types)
+        self.parameters = {
+            'delivery': delivery,
             'Q_p1': Q_p1,
             'V_c': V_c,
             'V_p1': V_p1,
@@ -33,12 +35,15 @@ class Model:
             'X': X,
         }
 
-    def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-    q_c, q_p1 = y
-    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-    dqp1_dt = transition
-    return [dqc_dt, dqp1_dt]
+def dose(t, X):
+    return X
+
+    def rhs(self, t, y):
+        q_c, q_p1 = y
+        transition = self.parameters["Q_p1"] * (q_c / self.parameters["V_c"] - q_p1 / self.parameters["V_p1"])
+        dqc_dt = dose(t, X) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
+        dqp1_dt = transition
+        return [dqc_dt, dqp1_dt]
 
    
 
