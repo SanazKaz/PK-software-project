170037e646b5eeeef28ce6eee6a8e3817e9c6f8c
Milan Sobota <milanh.b.sobota@gmail.com>
2023-10-18 16:48:53 +0100

    Completed basic model functionality and added some tests

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -2,27 +2,33 @@
 # Model class
 #
 
+import scipy
+import numpy as np
 class Model:
     """A Pharmokinetic (PK) model
 
     Parameters
     ----------
-
+    dose_function: function
+        not sure what this should look like #TODO
     Q_p1: numeric, optional
         the transition rate between central compartment and peripheral compartment 1
     V_c: numeric, optional
         [mL], the volume of the central compartment
     V_p1: numeric, optional
         [mL], the volume of the first peripheral compartment
+    k_a: numeric, optional
+        [mL], the volume of the first peripheral compartment
     CL: numeric, optional
         [mL/h], the clearance/elimination rate from the central compartment
     X: numeric, optional
-        an example paramter
+        [ng] the dose? Should this even be here?
     
 
     """
 
-    def __init__(self, dose_function, Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, X = 1.0, delivery = "intravenous"):
+    def __init__(self, dose_function, 
+                 Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, k_a = 1.0, delivery = "intravenous"):
         delivery_types = ['intravenous', 'subcutaneous']
         if delivery not in delivery_types:
             raise ValueError("Invalid delivery type. Expected one of: %s" % delivery_types)
@@ -32,26 +38,43 @@ class Model:
             'V_c': V_c,
             'V_p1': V_p1,
             'CL': CL,
-            'X': X,
+            'k_a': k_a
         }
-        self.dose = dose_function
+        self.dose_function = dose_function
 
 
     def rhs_iv(self, t, y):
         q_c, q_p1 = y
         transition = self.parameters["Q_p1"] * (q_c / self.parameters["V_c"] - q_p1 / self.parameters["V_p1"])
-        dqc_dt = self.dose(t, self.parameters["X"]) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
+        dqc_dt = self.dose_function(t) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt]
     
     def rhs_sc(self, t, y):
-        q_c, q_p1 = y
+        q_c, q_p1, q_p0 = y
         transition = self.parameters["Q_p1"] * (q_c / self.parameters["V_c"] - q_p1 / self.parameters["V_p1"])
-        dqp0_dt = self.dose(t, self.parameters["X"]) - self.parameters["V_c"] * q_c
-        dqc_dt = self.dose(t, self.parameters["X"]) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
+        dqp0_dt = self.dose_function(t) - self.parameters["k_a"] * q_p0
+        dqc_dt = self.parameters["k_a"] * q_p0 - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
         dqp1_dt = transition
-        return [dqc_dt, dqp1_dt]
+        return [dqc_dt, dqp1_dt, dqp0_dt]
     
+    def solve(self, t0 = 0, t1 = 1, steps = 1000):
+        t_eval = np.linspace(t0, t1, steps)
+        if(self.parameters["delivery"] == "intravenous"):
+            y0 = np.array([0.0, 0.0])
+            sol = scipy.integrate.solve_ivp(
+                fun=lambda t, y: self.rhs_iv(t, y),
+                t_span=[t_eval[0], t_eval[-1]],
+                y0=y0, t_eval=t_eval
+            )
+        elif(self.parameters["delivery"] == "subcutaneous"):
+            y0 = np.array([0.0, 0.0, 0.0])
+            sol = scipy.integrate.solve_ivp(
+                fun = lambda t, y: self.rhs_sc(t, y),
+                t_span=[t_eval[0], t_eval[-1]],
+                y0=y0, t_eval=t_eval
+            )
+        return sol
 
 
    
--- pkmodel/tests/test_model.py
+++ pkmodel/tests/test_model.py
@@ -6,10 +6,27 @@ class ModelTest(unittest.TestCase):
     """
     Tests the :class:`Model` class.
     """
+    def dose_dummy(self, t):
+        return 1.0
+
     def test_create(self):
         """
         Tests Model creation.
         """
-        model = pk.Model()
-        self.assertEqual(model.value, 42)
+        model = pk.Model(self.dose_dummy)
+        self.assertEqual(model.parameters["CL"], 1.0)
+        #self.assertEqual(model)
 
+    def test_model_defaults(self):
+        """
+        Tests Model defaults run and the solver returns successfully.
+        """
+        for i in ["intravenous", "subcutaneous"]:
+            with self.subTest(i=i):
+                model = pk.Model(self.dose_dummy, delivery = i)
+                sol = model.solve()
+                self.assertTrue(sol.success)
+        #self.assertEqual(model)
+
+if __name__ == '__main__':
+    unittest.main()
