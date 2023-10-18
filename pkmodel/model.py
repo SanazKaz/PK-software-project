28a7f4d85b60c424f0c95119987556cde7bad5fd
Milan Sobota <milanh.b.sobota@gmail.com>
2023-10-18 11:51:24 +0100

    Moved basic model to model.py

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -12,6 +12,33 @@ class Model:
         an example paramter
 
     """
-    def __init__(self, value=42):
+
+    model1_args = {
+        'name': 'model1',
+        'Q_p1': 1.0,
+        'V_c': 1.0,
+        'V_p1': 1.0,
+        'CL': 1.0,
+        'X': 1.0,
+    }
+
+    def __init__(self, Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, X = 1.0):
         self.value = value
+        model1_args = {
+            'name': 'model1',
+            'Q_p1': Q_p1,
+            'V_c': V_c,
+            'V_p1': V_p1,
+            'CL': CL,
+            'X': X,
+        }
+
+    def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+    q_c, q_p1 = y
+    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
+    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
+    dqp1_dt = transition
+    return [dqc_dt, dqp1_dt]
+
+   
 
