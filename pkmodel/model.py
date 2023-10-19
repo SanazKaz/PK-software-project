49341d2d2104719bdb302d5578edef3ceb598808
Antonio Mastromarino <antonio.v.mastromarino@gmail.com>
2023-10-19 17:20:06 +0100

    added new solver to the model

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -4,8 +4,13 @@
 
 import scipy
 import numpy as np
+import protocol
+import solution
+
 class Model:
-    """A Pharmokinetic (PK) model
+
+    """
+    A Pharmokinetic (PK) model
 
     Parameters
     ----------
@@ -21,16 +26,10 @@ class Model:
         [mL], the volume of the first peripheral compartment
     CL: numeric, optional
         [mL/h], the clearance/elimination rate from the central compartment
-    X: numeric, optional
-        [ng] the dose? Should this even be here?
-    
-
     """
 
-    def __init__(self, dose_function, 
-                 Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, k_a = 1.0, delivery = "intravenous"):
-        delivery_types = ['intravenous', 'subcutaneous']
-        if delivery not in delivery_types:
+    def __init__(self, Q_p1 = 1.0, V_c = 1.0, V_p1 = 1.0, CL = 1.0, k_a = 1.0, delivery = "intravenous"):
+        if delivery not in ['intravenous', 'subcutaneous']:
             raise ValueError("Invalid delivery type. Expected one of: %s" % delivery_types)
         self.parameters = {
             'delivery': delivery,
@@ -43,32 +42,32 @@ class Model:
         self.dose_function = dose_function
 
 
-    def rhs_iv(self, t, y):
+    def rhs_iv(self, t, y, protocol):
         q_c, q_p1 = y
         transition = self.parameters["Q_p1"] * (q_c / self.parameters["V_c"] - q_p1 / self.parameters["V_p1"])
-        dqc_dt = self.dose_function(t) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
+        dqc_dt = protocol.dose_function(t) - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt]
     
-    def rhs_sc(self, t, y):
+    def rhs_sc(self, t, y, protocol):
         q_c, q_p1, q_p0 = y
         transition = self.parameters["Q_p1"] * (q_c / self.parameters["V_c"] - q_p1 / self.parameters["V_p1"])
-        dqp0_dt = self.dose_function(t) - self.parameters["k_a"] * q_p0
+        dqp0_dt = protocol.dose_function(t) - self.parameters["k_a"] * q_p0
         dqc_dt = self.parameters["k_a"] * q_p0 - q_c / self.parameters["V_c"] * self.parameters["CL"] - transition
         dqp1_dt = transition
         return [dqc_dt, dqp1_dt, dqp0_dt]
     
-    def solve(self, t0 = 0, t1 = 1, steps = 1000):
+    def solve_steady(self, t0 = 0, t1 = 1, steps = 1000, y0 = None, protocol):
         t_eval = np.linspace(t0, t1, steps)
         if(self.parameters["delivery"] == "intravenous"):
-            y0 = np.array([0.0, 0.0])
+            if y0 == None: y0 = np.array([0.0, 0.0])
             sol = scipy.integrate.solve_ivp(
-                fun=lambda t, y: self.rhs_iv(t, y),
-                t_span=[t_eval[0], t_eval[-1]],
+                fun = lambda t, y: self.rhs_iv(t, y),
+                t_span = [t_eval[0], t_eval[-1]],
                 y0=y0, t_eval=t_eval
             )
         elif(self.parameters["delivery"] == "subcutaneous"):
-            y0 = np.array([0.0, 0.0, 0.0])
+            if y0 == None: y0 = np.array([0.0, 0.0, 0.0])
             sol = scipy.integrate.solve_ivp(
                 fun = lambda t, y: self.rhs_sc(t, y),
                 t_span=[t_eval[0], t_eval[-1]],
@@ -76,6 +75,33 @@ class Model:
             )
         return sol
 
+    def solve(self, t0 = 0, t1 = 1, steps = 1000, protocol):
+        remaining_steps = steps
+        protocol.sort_instanteneous_application(t0)
+        t_old = t0
+        Y = 0
+        t_new, X = protocol.next_application()
+        while(X != None and t_new < t1):
+            temp_steps = int(remaining_steps*(t_new - t_old)/(t1 - t_old))
+            sol = self.solve_steady(t_old, t_new, temp_steps + 1, Y, protocol)
+            remaining_steps -= temp_steps
+            t_solutions += [sol.t]
+            y_solutions += [sol.y]
+            t_old = sol.t[-1]
+            Y = sol.y[-1] + X
+            t_new, X = protocol.next_application()
+        sol = self.solve_steady(t_old, t1, remaining_steps + 1, Y, protocol)
+        t_solutions += [sol.t]
+        y_solutions += [sol.y]
+        t_sol = concatenate(t_solutions)
+        y_sol = concatenate(y_solutions, axis=-1)
+        return Solution(t_sol, y_sol, delivery)
 
-   
+        
+
+
+
+
+
+
 
