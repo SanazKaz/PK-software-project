2ae21b2e2be855c896b38c7f87cba09e74a5544c
Iris Marmouset <irismarmouset@gmail.com>
2023-10-19 17:22:23 +0100

    updated the protocol and wrote the tests for the protocol

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -21,47 +21,32 @@ class Protocol:
                  Defines the way we inject the drug: intravenous or subcutaneous 
 
                  
-    instantaneous_application: list of tuples, optionnal
+    instantaneous_application: list of tuples, optional
+                               Assumption: Sorted by time, first injection inside the [t0, t1] time interval
                                [(time point : dose injected at this timepoint)]
                                Defines the instantaneous injections of specific doses of X ng of the drug at one or more time points
 
 
-    steady_application: list, optionnal
-                        [Timepoint_0, Timepoint_1]
+    steady_application: tuple, optional
+                        (t0, t1)
                         Defines the timepoints of the beginning and the end of the steady application
     
                         
-    steady_dose = numeric, optionnal
+    steady_dose = numeric, optional
                    Defines the amount of drug injected through the steady application, between Timepoint_0 and Timepoint_1
 
     """
-    def __init__(self, type_dosing = "intravenous", instantaneous_application = [], steady_application = [t0 = 0, t1 = 1], steady_dose = 0):
+    def __init__(self, type_dosing = "intravenous", instantaneous_application = [], steady_application = (0,1), steady_dose = 0):
         self.type_dosing = type_dosing
         self.instantaneous_application = instantaneous_application
         self.steady_application = steady_application
         self.steady_dose = steady_dose
+        self.current = 0
 
-
-    def sort_instanteneous_application(self, t0 = 0):
-        """ A method to sort the instantenous application list according to the time 
-            and giving the index of the current application in this list 
-
-        Parameters
-        ----------
-
-        t0: float, optionnal
-            beginning of the experiement
-        
-        
-        """
-
-        self.instantaneous_application.sort()
-        while self.instantaneous_application[0] < t0:
-            self.current += 1
-    
 
     def next_application(self):
         """ A method updating the index of the application from the instantaneous_application list over time
+            And returning the tuple (time, dose) of the current instantaneous application
             
         """
 
@@ -81,7 +66,7 @@ class Protocol:
            time
         """
 
-        if self.steady_application[0] < t < self.steady_application[1] :
+        if (self.steady_application[0] <= t) and (t <= self.steady_application[1]) :
             return self.steady_dose
         else:
             return 0
