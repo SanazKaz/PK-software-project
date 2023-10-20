b4a0f6fb1c0d38ca3f5b1bd2d1c7dcd67aea1714
Milan Sobota <milanh.b.sobota@gmail.com>
2023-10-20 15:56:39 +0100

    Fixed solution test

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -10,6 +10,19 @@ class SolutionTest(unittest.TestCase):
         """
         Tests Solution creation.
         """
-        model = pk.Solution()
-        self.assertEqual(model.value, 44)
+        t_sol = [0.,1.]
+        y_sol = [[0.1, 0.2],[0.3,0.4]]
+        delivery = "intravenous"
+        solution = pk.Solution(t_sol, y_sol, delivery)
+        self.assertEqual(solution.t_sol, t_sol)
+
+    def test_plot(self):
+        """
+        Tests Solution creation.
+        """
+        t_sol = [0.,1.]
+        y_sol = [[0.1, 0.2],[0.3,0.4]]
+        delivery = "intravenous"
+        solution = pk.Solution(t_sol, y_sol, delivery)
+        solution.plot_data()
 
